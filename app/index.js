2e3d15aa288ecb99f1a510372bd1b4dbf7495c40
Hanif Bahari <hanif.bahari@gmail.com>
2023-04-14 11:20:28 +0200

    app structure

---
+++ app/index.js
@@ -0,0 +1,26 @@
+import { View, ScrollView, SafeAreaView } from 'react-native';
+import { useState } from 'react';
+import { Stack, useRouter } from 'expo-router';
+
+import { COLORS, icons, images, SIZES } from '../constants';
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from '../components';
+
+const Home = () => {
+  const router = useRouter();
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        option={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+        }}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default Home;
